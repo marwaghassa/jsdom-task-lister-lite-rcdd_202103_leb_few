990bbf866c15c1c868dec3fcb13e35b64b0b510a
Marwa Ghassa <marwaghassa7@gmail.com>
2021-04-19 20:13:47 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 
   // your code here
-  /*let newTaskForm = document.getElementById("create-task-form");
-  let newTaskDescription = document.getElementById("new-task-description");
-  let submit=document.getElementByTagName('input')[1]; 
-  
-  const newTaskUl = document.getElementById("tasks");*/
+ 
   
  //- As a user, I should be able to type a task into the input field.
  
@@ -15,47 +11,3 @@
 //- As a user, the task string that I provided should appear on the DOM after the submit button has been activated.
 
 
-document.addEventListener("DOMContentLoaded", () => {
-  //grab all the necessary DOM elements
-
-  //form and relevant input fields
-  const newTaskForm = document.getElementById("create-task-form");
-  const newTaskDescription = document.getElementById("new-task-description");
-  const newTaskPriority = document.getElementById("new-task-priority");
-
-  //ul where new tasks will live on the DOM
-  const newTaskUl = document.getElementById("tasks");
-
-  //attach event listeners
-  newTaskForm.addEventListener("submit", createNewTask);
-});
-
-const createNewTask = event => {
-  event.preventDefault();
-  //stop form from trying to submit
-  const newTaskDescription = document.getElementById("new-task-description");
-  const newTask = document.createElement("li");
-  newTask.innerText = newTaskDescription.value;
-
-  appendNewTask(newTask);
-  event.target.reset();
-};
-
-const appendNewTask = task => {
-  document.getElementById("tasks").appendChild(task);
-};
-
-const createNewTask = event => {
-  event.preventDefault();
-  //stop form from trying to submit
-  const newTaskDescription = document.getElementById("new-task-description");
-  const newTask = document.createElement("li");
-  newTask.innerText = newTaskDescription.value;
-
-  appendNewTask(newTask);
-  event.target.reset();
-};
-
-const appendNewTask = task => {
-  document.getElementById("tasks").appendChild(task);
-};
